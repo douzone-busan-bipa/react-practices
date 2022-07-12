fa484cc8eb7c8fb13520b0a0c60e927163aa6fdb
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-07-12 16:18:22 +0900

    React Basic ex01

---
+++ 1.basics/ex01/public/index.js
@@ -0,0 +1,10 @@
+function App() {
+    const App = document.createElement('div');
+    App.textContent = "Hello World";
+    return App;
+}
+
+document
+    .getElementById('root')
+    .appendChild(App());
+
