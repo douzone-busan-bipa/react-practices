3f9ea59c4ac3299f312f117c49388f3a3e84a392
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-07-13 17:04:04 +0900

    react api

--- 1.basics/ex05/src/index.js
+++ 1.basics/ex05/src/index.js
@@ -1,4 +1,8 @@
+import ReactDOM from 'react-dom';
 import {App} from './App.js';
-document
-    .getElementById('root')
-    .appendChild(App());
+
+// document
+//     .getElementById('root')
+//     .appendChild(App());
+ReactDOM.rend
+er(App(), document.getElementById('root'));
---
+++ 1.basics/ex06/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+    // const App = React.createElement('div', null, 'Hello World');
+    // return App;
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export {App}
