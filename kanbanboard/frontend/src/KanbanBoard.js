69968a3c87d2e24ff0d7f4a926599594e4c61f7c
더존비즈온 <103369445+douzone-busan-bipa@users.noreply.github.com>
2022-07-21 09:24:17 +0900

    Update KanbanBoard.js

--- kanbanboard/frontend/src/KanbanBoard.js
+++ kanbanboard/frontend/src/KanbanBoard.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import cards from './assets/json/data.json'
+import styles from './assets/css/KabanBoard.css';
 import CardList from './CardList';
+import cards from './assets/json/data.json'
 
 function KanbanBoard() {
-    return (
-        <div className={'KanbanBoard'}>
-            <CardList title={'ToDo'} cards={cards.filter(e => e.status === 'ToDo')}/>
-            <CardList title={'Doing'} cards={cards.filter(e => e.status === 'Doing')}/>
-            <CardList title={'Done'} cards={cards.filter(e => e.status === 'Done')}/>
+return (
+        <div className={styles.KanbanBoard}>
+            <CardList
+                key='ToDo'
+                title='ToDo'
+                cards={cards.filter(card => card.status == 'ToDo')} />
+
+            <CardList
+                key='Doing'
+                title='Doing'
+                cards={cards.filter(card => card.status == 'Doing')} />
+
+            <CardList
+                key='Done'
+                title='Done'
+                cards={cards.filter(card => card.status == 'Done')} />
         </div>
     );
 }
