b32dd943bd6501110e122047041acac0979672e2
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-07-22 10:15:00 +0900

    data flow: down -> bottom

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
 import Emaillist from './Emaillist';
-import emails from './assets/json/data.json'
+import data from './assets/json/data.json'
 import './assets/scss/App.scss';
 
 const App = () => {
+  const [emails, setEmails] = useState(data);
+  
+  const notifyEmailAdd = function(email) {
+    console.log(email);
+  }
+
+  const notifyKeywordChanged = function(keyword) {
+    console.log(keyword);
+  }
+
   return (
     <div id={'App'}>
       <RegisterForm />
-      <SearchBar />
+      <SearchBar callback={notifyKeywordChanged}/>
       <Emaillist emails={emails} />
     </div>
   )
--- emaillist/frontend/src/SearchBar.js
+++ emaillist/frontend/src/SearchBar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from './assets/scss/SearchBar.scss';
 
-export default function SearchBar() {
+export default function SearchBar({callback}) {
   return (
     <div className={styles.Searchbar}>
         <input
           type='text'
-          placeholder='찾기' />
+          placeholder='찾기' 
+          onChange={ e => {
+            callback(e.target.value);
+          }}/>
     </div>
   )
 }
