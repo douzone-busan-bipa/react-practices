5265004325db513c0f94a286105e98093ee3a884
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-07-29 10:24:50 +0900

    mysite07

--- mysite07/backend/src/main/java/me/kickscar/mysite/exception/GlobalExceptionHandler.java
+++ mysite07/backend/src/main/java/me/kickscar/mysite/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package me.kickscar.emailllist.exception;
+package me.kickscar.mysite.exception;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -10,18 +10,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
-import me.kickscar.emailllist.dto.JsonResult;
+import me.kickscar.mysite.dto.JsonResult;
 
-@Slf4j
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+	
 	@ExceptionHandler(Exception.class)
 	@ResponseBody
 	public ResponseEntity<JsonResult> handlerException(Exception e) {
 		// 1. 로깅(logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		log.info(errors.toString());
+		log.error(errors.toString());
 		
 		// 2. JSON 응답
 		return ResponseEntity.status(HttpStatus.OK).body(JsonResult.fail(errors.toString()));
---
+++ mysite07/backend/src/test/java/me/kickscar/mysite/MysiteApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kickscar.mysite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
