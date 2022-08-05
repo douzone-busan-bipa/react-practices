64318ba86c8aec85312703988313d903a3adfd83
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-08-05 11:14:00 +0900

    deploy#9

--- kanbanboard/backend/src/main/resources/script/launch.sh
+++ kanbanboard/backend/src/main/resources/script/launch.sh
@@ -13,4 +13,4 @@ fi
 
 echo "starting [$APPLICATION_NAME]"
 cd $SCRIPT_DIR
-nohup /usr/local/douzone/java/bin/java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$APPLICATION_NAME.jar >> $SCRIPT_DIR/launch.log &
+nohup java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$APPLICATION_NAME.jar >> $SCRIPT_DIR/launch.log &
